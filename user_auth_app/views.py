f16a7d1a1bdb55ee66c54c62c0bf3b0f6bebbb85
unknown <leonjasnoszumi78@gmail.com>
2019-07-22 13:58:31 -0700

    Refactore UserLogin View

--- user_auth_app/views.py
+++ user_auth_app/views.py
@@ -1,30 +1,31 @@
 from django.shortcuts import render, reverse, redirect
 from django.views import View
+from django.views.generic.edit import FormView
 from user_auth_app.forms import UserLoginForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-class UserLogin(View):
-    def get(self, request):
-        form = UserLoginForm
-        return render(request, 'user_auth_app/forms/user_login_form.html', { 'form' : form })
-    def post(self, request):
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('quests_manager:index')
+class UserLogin(FormView):
+    template_name = 'user_auth_app/forms/user_login_form.html'
+    form_class = UserLoginForm
+    success_url = 'quests'
+
+    def form_valid(self, form):
+        #Getting data from Form
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        #Checking credentials
+        user = authenticate(username=username, password=password)
+        if user:
+            return super().form_valid(form)
         else:
-            form = UserLoginForm
             return redirect('user_auth_app:login')
 
-class UserRegister(View):
+class UserRegister(FormView):
     template_name = 'user_auth_app/forms/user_register_form.html'
-    def get(self, request):
-        form = UserCreationForm
-        return render(request, self.template_name, {'form' : form})
+    form_class = UserCreationForm
+
     def post(self, request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
