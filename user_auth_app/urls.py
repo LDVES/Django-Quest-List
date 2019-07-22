78ded620bf2f7e3b417f9daecd862bfbdf3a1311
unknown <leonjasnoszumi78@gmail.com>
2019-07-21 19:07:44 -0700

    Added Registration View

--- user_auth_app/urls.py
+++ user_auth_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from user_auth_app.views import UserLogin
+from user_auth_app.views import UserLogin, UserRegister
 
 #Namespace for quests manager
 app_name='user_auth_app'
 urlpatterns = [
 	path('login', UserLogin.as_view(), name='login'),
-    #path('register', QuestsList.as_view(), name=''),
+    path('register', UserRegister.as_view(), name='register'),
 ]
--- user_auth_app/views.py
+++ user_auth_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, reverse, redirect
 from django.views import View
 from user_auth_app.forms import UserLoginForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 class UserLogin(View):
@@ -18,3 +19,20 @@ class UserLogin(View):
         else:
             form = UserLoginForm
             return redirect('user_auth_app:login')
+
+class UserRegister(View):
+    template_name = 'user_auth_app/forms/user_register_form.html'
+    def get(self, request):
+        form = UserCreationForm
+        return render(request, self.template_name, {'form' : form})
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('user_auth_app:login')
+        else:
+            return redirect('user_auth_app:register')
