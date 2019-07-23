69718c28d76b5e99c7fcbd222e99f70cfa918212
unknown <leonjasnoszumi78@gmail.com>
2019-07-23 12:32:58 -0700

    Added You are not logged message in QuestList in views.py

--- quests_manager/views.py
+++ quests_manager/views.py
@@ -1,5 +1,7 @@
 #View stuff
 from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.views.generic import (
 ListView,
 CreateView,
@@ -10,9 +12,7 @@ UpdateView,
 
 #DB stuff
 from .models import Quest
-
 #Quest CRUD
-
 
 class QuestsList(ListView):
     model = Quest
@@ -27,6 +27,13 @@ class QuestCreate(CreateView):
     model = Quest
     fields = ['title', 'body']
     template_name = 'quests_manager/forms/quest_create_form.html'
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return render(request, self.template_name, { 'form' : self.form })
+        else:
+            messages.add_message(request, messages.ERROR, 'You are not logged in')
+            return redirect('quests_manager:index')
 
 class QuestDelete(DeleteView):
     model = Quest
