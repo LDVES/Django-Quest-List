8fb9be1107f7c97170087467d1a7a301cf944b86
LDVES <leonjasnoszumi78@gmail.com>
2019-07-24 10:04:17 +0200

    Added LoginRequiredMixin to views.py

--- quests_manager/views.py
+++ quests_manager/views.py
@@ -2,7 +2,7 @@
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from django.views.generic import (
 ListView,
@@ -16,34 +16,31 @@ UpdateView,
 from .models import Quest
 
 #Checking if user is authenticated
-class TestIfUserIsAuthenticated(UserPassesTestMixin, View):
+class CheckIfUserIsAuthenticated(LoginRequiredMixin, View):
     login_url='home:index'
-
-    def test_func(self):
-        return self.request.user.is_authenticated
 
 #Quest CRUD
 
-class QuestsList(TestIfUserIsAuthenticated, ListView):
+class QuestsList(CheckIfUserIsAuthenticated, ListView):
     model = Quest
     context_object_name = 'quests'
     template_name = 'quests_manager/index.html'
 
-class QuestDetail(TestIfUserIsAuthenticated, DetailView):
+class QuestDetail(CheckIfUserIsAuthenticated, DetailView):
     model = Quest
     template_name = 'quests_manager/detail.html'
 
-class QuestCreate(TestIfUserIsAuthenticated, CreateView):
+class QuestCreate(CheckIfUserIsAuthenticated, CreateView):
     model = Quest
     fields = ['title', 'body']
     template_name = 'quests_manager/forms/quest_create_form.html'
 
-class QuestDelete(TestIfUserIsAuthenticated, DeleteView):
+class QuestDelete(CheckIfUserIsAuthenticated, DeleteView):
     model = Quest
     template_name = 'quests_manager/delete.html'
     success_url = reverse_lazy("quests_manager:index")
 
-class QuestUpdate(TestIfUserIsAuthenticated, UpdateView):
+class QuestUpdate(CheckIfUserIsAuthenticated, UpdateView):
     model = Quest
     fields = ['title', 'body']
     template_name = "quests_manager/forms/quest_update_form.html"
