31934cf94b3c5c7201b80b162278556d112657fe
unknown <leonjasnoszumi78@gmail.com>
2019-07-28 11:23:31 -0700

    Added Custom Auth Middleware

---
+++ user_auth_app/middleware/UserAuthMiddleware.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render,  redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+class UserAuthMiddleware():
+
+    #Redirects user to redirect_url when user is authenticated
+    def redirect_user_if_authenticated(self, request):
+       if request.user.is_authenticated:
+           return redirect(self.redirect_url)
+       else:
+           return render(request, self.template_name, { 'form' : self.form_class })
+
+
+    #Getting data from form and logging user
+    def login_user(self, form):
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        #Checking credentials
+        user = authenticate(username=username, password=password)
+        if user:
+            login(self.request, user)
+            return redirect(self.success_url)
+        else:
+            #Invalid credentials message
+            messages.add_message(self.request, messages.ERROR, 'Invalid credentials' )
+            return redirect('user_auth_app:login')
+
+
+    #Registering user
+    def register_user(self, form):
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username = username, password = password)
+        form.save()
+        return redirect(self.success_url)
--- user_auth_app/views.py
+++ user_auth_app/views.py
@@ -1,62 +1,44 @@
-from django.shortcuts import render,  redirect
 from django.urls import reverse_lazy
-from django.views import View
+
 from django.views.generic.edit import FormView
 from user_auth_app.forms import UserLoginForm
-from django.contrib import messages
-from django.contrib.auth import authenticate, login
+
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import HttpResponseRedirect
 from django.contrib.auth import logout
+
+#Custom Middleware
+from .middleware.UserAuthMiddleware import UserAuthMiddleware
+
 
 # Create your views here.
 class UserLogin(FormView):
     template_name = 'user_auth_app/forms/user_login_form.html'
     form_class = UserLoginForm
+    #Dependency Incjection of UserAuthMiddleware
+    user_auth_middleware = UserAuthMiddleware
+    #URL to redirect authenticated users
+    redirect_url = 'quests_manager:index'
     success_url = reverse_lazy('quests_manager:index')
 
     #Checking if user is logged
     def get(self, request):
-       if request.user.is_authenticated:
-           return redirect('quests_manager:index')
-       else:
-           return render(request, self.template_name, { 'form' : self.form_class })
+       return self.user_auth_middleware.redirect_user_if_authenticated(self, self.request)
 
     def form_valid(self, form):
-        #Getting data from Form
-        username = form.cleaned_data['username']
-        password = form.cleaned_data['password']
-        #Checking credentials
-        user = authenticate(username=username, password=password)
-        if user:
-            login(self.request, user)
-            return super().form_valid(form)
-        else:
-            #Invalid credentials message
-            messages.add_message(self.request, messages.ERROR, 'Invalid credentials' )
-            return redirect('user_auth_app:login')
+       return self.user_auth_middleware.login_user(self, form)
 
-class UserRegister(FormView):
+class UserRegister(UserLogin):
     template_name = 'user_auth_app/forms/user_register_form.html'
     form_class = UserCreationForm
-    success_url = reverse_lazy('user_auth_app:login')
 
-    #Checking if user is logged
     def get(self, request):
-        if request.user.is_authenticated:
-            return redirect('quests_manager:index')
-        else:
-            return render(request, self.template_name, { 'form' : self.form_class })
+        return self.user_auth_middleware.redirect_user_if_authenticated(self, self.request)
 
     #Getting data from Form and processing
     def form_valid(self, form):
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
-        user = authenticate(username = username, password = password)
-        form.save()
-        return super().form_valid(form)
+        return self.user_auth_middleware.register_user(self, form)
 
-#Loggin out user
+#Logging out user
 def logout_view(request):
     logout(request)
     return redirect('home:index')
